29486c0c1c26d5158f0f2d4fed4fd94193cca039
raviraj <neoravikumar@gmail.com>
2025-11-13 22:07:01 +0530

    13-Nov-2025

--- src/main/java/com/ravi/leetcode/binarySearch/BinarySearch.java
+++ src/main/java/com/ravi/leetcode/binarySearch/BinarySearch.java
@@ -1,4 +1,38 @@
 package com.ravi.leetcode.binarySearch;
 
+import java.util.OptionalInt;
+
 public class BinarySearch {
+    static void main() {
+        int[] nums = {-1, 2, 9, 41, 55, 98};
+        int target = 41;
+        int result = search(nums, target);
+        System.out.println("Result is :>>" + result);
+    }
+
+    public static int search(int[] nums, int target) {
+        var result = binarySearch(nums, target, 0, nums.length - 1);
+        return result.getAsInt();
+
+    }
+
+
+    static OptionalInt binarySearch(int[] arr, int target, int left, int right) {
+        if (left > right) {
+            return OptionalInt.empty();
+        }
+        int mid = left + (right - left) / 2;
+
+        return switch (Integer.compare(arr[mid], target)) {
+            case 0 -> OptionalInt.of(mid);
+            case -1 -> binarySearch(arr, target, mid + 1, right);
+            case 1 -> binarySearch(arr, target, left, mid - 1);
+            default -> OptionalInt.empty();
+        };
+    }
+
 }
+
+
+
+
